312c39771de3ee26343c00cdd8e275ec7c28bf5b
shafali03 <shaf03sm@gmail.com>
2020-05-18 18:35:45 +0100

    feat: Created a responsive landing page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
-const navToggle = document.querySelector('.nav-toggle')
-const nav = document.querySelector('.nav');
 
-navToggle.addEventListener('click', () => {
-  nav.classList.toggle('nav--visible')
-})
+/* =========================================
+                Navigation
+============================================ */
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+let navIconToggle = document.querySelector("#nav-icon");
+
+navBarToggle.addEventListener('click', () => {
+  mainNav.classList.toggle('active');
+  mainNav.classList.add('smNav')
+
+});
+
+navIconToggle.addEventListener("click", () => {
+  navIconToggle.classList.toggle("active"); mainNav.classList.remove('smNav')
+
+});
+
+
+/* =========================================
+                Navigation scroll
+============================================ */
+
+
+window.addEventListener('scroll', () => {
+
+
+  const nav = document.querySelector('nav');
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+  // const aboutText = document.querySelector('.about__text');
+
+
+  if (Math.ceil(scrolled) === scrollable || scrolled > 100) {
+    nav.classList.add('sticky');
+  }
+
+  else {
+    (scrolled === scrollable || scrolled < 0)
+    nav.classList.remove('sticky');
+    nav.classList.add('noSticky')
+
+  }
+});
+
